2d49e7307c57c5b79fa703e208b71accd2b95bf3
Peter <pnolan1986@gmail.com>
2017-07-12 14:35:38 -0400

    Files are now writing output in seemingly correct format, just need to add loop to go through all input files

--- degrib.py
+++ degrib.py
@@ -1,149 +1,76 @@
 import numpy, types
-#from scipy.interpolate import SmoothSphereBivariateSpline as sinterp
-#from scipy.interpolate import RectSphereBivariateSpline as sinterp
+
 import Nio
 import Ngl
 
-file = Nio.open_file("../gribs/nam_218_20160609_0000_000.grb2","r,")
-names = file.variables.keys()
+def main():
+	file = Nio.open_file("../gribs/nam_218_20160609_0000_000.grb2","r,")
+	names = file.variables.keys()
 
-'''
-for i in range(len(names)):
-	print "\n" + names[i]
-	if names[i][:5]=="lv_IS":
-		print "\n" + names[i]
-		print file.variables[names[i]].dimensions
-		for attrib in file.variables[names[i]].attributes.keys():
-			print attrib + " has value ",  getattr(file.variables[names[i]],attrib)
+	# level 30 is the 800 millibar level
+	uvar = file.variables["UGRD_P0_L100_GLC0"][:,:,:]
+	uvar = numpy.squeeze(uvar[30,:,:])
+	dim = numpy.shape(uvar)
+	print dim
 
-
-level = file.variables["lv_ISBL0"]
-print numpy.shape(level)
-print file.variables["lv_ISBL0"].dimensions
-print level[30]
-'''
-
-uvar = file.variables["UGRD_P0_L100_GLC0"][:,:,:]
-uvar = numpy.squeeze(uvar[30,:,:])
-dim = numpy.shape(uvar)
-print dim
-#size = numpy.size(uvar)
-vvar = file.variables["VGRD_P0_L100_GLC0"][:,:,:]
-vvar = numpy.squeeze(vvar[30,:,:])
-
-'''
-xgrid = file.variables["xgrid_0"]
-print numpy.shape(xgrid)
-print xgrid
-
-ygrid = file.variables["ygrid_0"]
-print numpy.shape(ygrid)
-print ygrid
-'''
-
-lat = file.variables["gridlat_0"][:,:]
-lon = file.variables["gridlon_0"][:,:]
-# 36.75
-#120.875
-#Find the index of the origin
-'''
-for i in range(dim[0]):
-	for j in range(dim[1]):
-		if numpy.fabs(lat[i,j]-36.75)<0.075 and numpy.fabs(lon[i,j]+120.875)<0.075:
-			print lat[i,j], lon[i,j]
-			print i,j
-			iorg = i
-			jorg = j
-'''
-#hardcoded origin to save time
-iorg = 195
-jorg = 155
-#choose 42, want 1000km domain centered at (36.75,-120.875), with 12.191km gridspacing, 42 is roughly 500km
-latdes = lat[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
-londes = lon[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
-print latdes[41,41],londes[41,41]
-print latdes.shape
-#udesired = uvar[(iorg-42):(iorg+42),(jorg-42):(jorg+42)]
-#vdesired = vvar[(iorg-42):(iorg+42),(jorg-42):(jorg+42)]
-#print udesired.shape
+	vvar = file.variables["VGRD_P0_L100_GLC0"][:,:,:]
+	vvar = numpy.squeeze(vvar[30,:,:])
 
 
+	lat = file.variables["gridlat_0"][:,:]
+	lon = file.variables["gridlon_0"][:,:]
 
-#Here Lies attempt at regridding
-"""
-theta = (lat[:,:] + 90*numpy.ones(numpy.shape(lat)))*numpy.pi/180
-phi = (lon[:,:] + 360*numpy.ones(numpy.shape(lon)))*numpy.pi/180
+	#hardcoded origin to save time
+	iorg = 195
+	jorg = 155
+	#choose -41 +42, want 1000km domain centered at (36.75,-120.875), with 12.191km gridspacing, 83 is roughly 1000km
+	#latdes = lat[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
+	#londes = lon[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
+	#print latdes[83,41],londes[41,41]
+	#print latdes.shape
+	udesired = uvar[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
+	vdesired = vvar[(iorg-41):(iorg+42),(jorg-41):(jorg+42)]
+	dim =  udesired.shape
+	#f = open('roms.dat','w')
+	t=1
 
-'''
-Nlat =  max(lat[:,0])
-Slat =  min(lat[:,-1])
-Wlon =  min(lon[0,:])
-Elon =  max(lon[0,:])
-'''
+	#if t>=1000:
+	#	break
+	print(t)
+	filenumber=determineNumber(t)
+	zone=determineZone(t)
+	f = open('roms'+filenumber+'.dat', 'w')
+	f.write("Surface Velocity ROMS data (m/s)\n")
+	f.write("#Data_XMin = "+str(-1*(dim[1]-1)/2*12.191)+"\n")
+	f.write("#Data_XMax = "+str((dim[1]-1)/2*12.191)+"\n")
+	f.write("#Data_XRes = "+str(dim[1])+"\n")
+	f.write("#Data_YMin = "+str(-1*(dim[0]-1)/2*12.191)+"\n")
+	f.write("#Data_YMax = "+str((dim[0]-1)/2*12.191)+"\n")
+	f.write("#Data_YRes = "+str(dim[0])+"\n")
+	f.write("ZONE T=\""+zone+"\" I="+str(dim[1])+" J="+str(dim[0])+"\n")
+	for i in range(dim[0]):
+		for j in range(dim[1]):
+			f.write(str(udesired[i,j])+" "+str(vdesired[i,j])+"\n")
 
-Nth =  max(theta[:,0])
-Sth =  min(theta[:,-1])
-Wph =  min(phi[0,:])
-Eph =  max(phi[0,:])
-'''
-print "%.5f" % Nth
-print "%.5f" % Sth
-print "%.5f" % Wph
-print "%.5f" % Eph
+	f.close()
+	Ngl.end()
 
-print dim[0],dim[1]
-'''
-newtheta = numpy.linspace(Sth,Nth,dim[0])
-newphi = numpy.linspace(Wph,Eph,dim[1])
+def determineNumber(timestep):
+	if timestep <10:
+		w='000'+str(timestep)
+	elif timestep>=10 and timestep<100:
+		w='00'+str(timestep)
+	elif timestep>=100 and timestep<1000:
+		w='0'+str(timestep)
+	return w;
 
-newphi, newtheta = numpy.meshgrid(newphi,newtheta)
+def determineZone(timestep):
+	if timestep <10:
+		zone='000'+str(timestep+1)
+	elif timestep>=10 and timestep<100:
+		zone='00'+str(timestep+1)
+	elif timestep>=100 and timestep<1000:
+		zone='0'+str(timestep+1)
+	return zone;
 
-
-print numpy.shape(theta)
-print numpy.shape(newtheta)
-
-'''
-theta = numpy.reshape(theta,size)
-newtheta = numpy.reshape(newtheta,size)
-phi = numpy.reshape(phi,size)
-newphi = numpy.reshape(newphi,size)
-uvar = numpy.reshape(uvar[:,:],size)
-vvar = numpy.reshape(vvar[:,:],size)
-'''
-
-theta = numpy.ravel(theta)
-newtheta = numpy.ravel(newtheta)
-phi = numpy.ravel(phi)
-newphi = numpy.ravel(newphi)
-uvar = numpy.ravel(uvar[:,:])
-vvar = numpy.ravel(vvar[:,:])
-
-'''
-import sys
-sys.stdout.flush()
-print "Interp"
-uinterp = sinterp(theta,phi,uvar)
-print "Set"
-sys.stdout.flush()
-newu = uinterp(newtheta,newphi)
-print "Done"
-'''
-
-'''
-print theta
-print newtheta
-print phi
-print newphi
-'''
-
-
-
-
-'''
-for i in range(100):
-	for j in range(100):
-		print uvar[i,j], vvar[i,j], lat[i,j], lon[i,j]
-'''
-
-Ngl.end()
-"""
+main()
